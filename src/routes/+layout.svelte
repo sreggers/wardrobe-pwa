b01d924fed1d5bd70cf67799e67f9d8bc0375441
Sebastiaan <sebastiaan.reggers@jcore.com>
2023-06-12 16:53:45 +0200

    Add skeleton UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-hamlindigo.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<slot />
+</AppShell>
