24a91e8d4970dfc96987241ada2c27ef457db0e3
Sebastiaan <sebastiaan.reggers@jcore.com>
2023-06-18 17:02:00 +0200

    Add static files and use static adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <section class="container mx-auto p-8">
-    <h1 class="h1">Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <h1 class="h1">Welcome to Wardrobe</h1>
+    <p>Coming soon ... ish</p>
 </section>
